b795bc975a6d5130c5b70dbde7b0b003ce8852be
zc.zhou <zzc@renren.com>
2023-06-26 16:41:21 +0800

    stablediffusion

--- src/pages/api/text2img/stable-diffusion.ts
+++ src/pages/api/text2img/stable-diffusion.ts
@@ -1,9 +1,45 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
+const apiKey = process.env.STABLE_DIFFUSION_API_KEY;
+const url = 'https://api.stability.ai/v1/generation/stable-diffusion-v1-5/text-to-image';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse){
-  if(req.method === "POST"){
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    if (!apiKey) {
+      return res.status(500).json({
+        error: {
+          message: "API key not configured",
+        }
+      });
+    }
     // todo
-    res.end("use stable diffusion text2img api")
+    const { p, size = '256', count = '1', seed = 0, style } = req.body;
+    const body = {
+      text_prompts: [
+        {
+          text: p
+        },
+      ],
+      cfg_scale: 7,
+      clip_guidance_preset: 'FAST_BLUE',
+      height: size,
+      width: size,
+      samples: count,
+      steps: 30,
+      seed,
+    } as any;
+    if (style) {
+      body.style_preset = style;
+    }
+    const response = await axios.post(url, body, {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      }
+    }) as any;
+    console.log(`res ${JSON.stringify(response)}`)
+    res.status(200).json(response.artifacts)
   }
   res.status(400).end("Bad request")
 }
