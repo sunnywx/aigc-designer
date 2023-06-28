c68a46511df9ea0df795455fbbb54b94d5be3e0b
sunnywx <iwisunny@gmail.com>
2023-06-28 15:48:54 +0800

    feat: topbar zoomer add mode switch button
    Signed-off-by: sunnywx <iwisunny@gmail.com>

--- src/components/topbar/zoomer.tsx
+++ src/components/topbar/zoomer.tsx
@@ -1,8 +1,8 @@
 import { useEditor } from '@/editor'
 import styles from './index.module.scss'
 import { BiMinusCircle, BiPlusCircle } from 'react-icons/bi'
-import {PiHandGrabbing} from 'react-icons/pi'
-import { useCallback, useEffect, useMemo, useState } from "react";
+import {PiHandGrabbing, PiCursorFill} from 'react-icons/pi'
+import { useEffect, useMemo, useState } from "react";
 import ActionButton from '@/components/action-button'
 import cs from 'classnames'
 
@@ -15,15 +15,10 @@ export default function Zoomer(props: Props) {
   const zoomLabel=useMemo(()=> {
     return (parseInt(String((canvasState.zoom || 1)?.toFixed(2) * 100))) + '%'
   }, [canvasState.zoom])
+  const [mode, setMode]=useState<'select' | 'drag'>('select')
   
   function syncZoom(zoom?: number){
     setCanvasState(prev=> ({...prev, zoom: zoom ?? canvas?.canvas?.getZoom() as number}))
-  }
-  
-  const enableDragMode=()=> {
-    canvas?.canvas?.discardActiveObject()
-    canvas?.canvas?.requestRenderAll()
-    setCanvasState(prev=> ({...prev, dragMode: !prev.dragMode}))
   }
   
   useEffect(()=> {
@@ -31,8 +26,38 @@ export default function Zoomer(props: Props) {
     emitter.on('zoomChange', syncZoom)
   }, [])
   
+  useEffect(()=> {
+    if(mode === 'select'){
+      setCanvasState(prev=> ({...prev, dragMode: false}))
+    }
+    if(mode === 'drag'){
+      canvas?.canvas?.discardActiveObject()
+      canvas?.canvas?.requestRenderAll()
+      setCanvasState(prev=> ({...prev, dragMode: true}))
+    }
+  }, [mode])
+  
   return (
     <div className={styles.zoomer}>
+      <ActionButton
+        title='Select mode'
+        icon={<PiCursorFill size={22}/>}
+        onClick={()=> setMode('select')}
+        className={cs(styles.btnMode, {
+          [styles.active]: mode === 'select',
+          [styles.disable]: mode !== 'select'
+        })}
+      />
+      <ActionButton
+        title='Drag mode'
+        icon={<PiHandGrabbing size={22}/>}
+        onClick={()=> setMode('drag')}
+        className={cs(styles.btnMode, {
+          [styles.active]: mode === 'drag',
+          [styles.disable]: mode !== 'drag',
+        })}
+      />
+      
       <ActionButton title='Zoom in' icon={<BiPlusCircle size={24}/>} onClick={()=> {
         canvas?.zoomIn()
         syncZoom()
@@ -47,12 +72,6 @@ export default function Zoomer(props: Props) {
         canvas?.zoomOut()
         syncZoom()
       }} />
-      <ActionButton
-        title='Drag canvas'
-        icon={<PiHandGrabbing size={24}/>}
-        onClick={enableDragMode}
-        className={cs({[styles.active]: canvasState.dragMode})}
-      />
     </div>
   );
 }
